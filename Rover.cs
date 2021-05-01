6b29f0463f71878897e56ef32f2b86bfe8711bb1
Victor <71389732+Vitkof@users.noreply.github.com>
2021-05-01 12:33:16 +0300

    Create Rover.cs

---
+++ Rover.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Rover
+{
+    public static void CalculateRoverPath(int[,] map)
+    {
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
