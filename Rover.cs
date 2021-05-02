d5028717d6a719c70e2166449e8bc811be63493e
Victor <71389732+Vitkof@users.noreply.github.com>
2021-05-02 13:53:35 +0300

    +method Dejkstra. Initialization vars

--- Rover.cs
+++ Rover.cs
@@ -2,6 +2,16 @@ using System;
 
 public class Rover
 {
+    static Dictionary<string, string> Dejkstra(string startNode, string endNode, 
+        Dictionary<string, (int, string)[]> graph)
+    {
+        SortedList<int, string> nodesQueue = new SortedList<int, string>() { { 0, startNode } };
+        Dictionary<string, int> fuelVisit = new Dictionary<string, int>() { { startNode, 0 } };
+        Dictionary<string, string> visitedFrom = new Dictionary<string, string>() { { startNode, null } };
+        
+        return visitedFrom;
+    }
+    
     public static void CalculateRoverPath(int[,] map)
     {
     }
