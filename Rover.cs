64da7ad7faf8b9b5651a991df131ec1c3306a0f9
Victor <vitkow@tut.by>
2021-05-03 11:39:44 +0300

    Dejkstra: return Tuple. Calculate: refactoring delegate,+WorkDejkstra, +StreamWriter. Test

--- Rover.cs
+++ Rover.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public class Rover
 {
-    static Dictionary<string, string> Dejkstra(string startNode, string endNode,
+    static (Dictionary<string, string>, int) Dejkstra(string startNode, string endNode,
         Dictionary<string, (int, string)[]> graph)
     {
         List<(int, string)> nodesQueue = new List<(int, string)>() { (0, startNode) };
@@ -34,7 +35,7 @@ public class Rover
             }
         }
 
-        return visitedFrom;
+        return (visitedFrom, fuelVisit[endNode]);
     }
 
     delegate bool CheckNextNode(int x, int y);
@@ -44,42 +45,64 @@ public class Rover
         int cols = map.GetLength(1);
         Dictionary<string, (int, string)[]> graph = new Dictionary<string, (int, string)[]>();
 
-        CheckNextNode check = delegate (int i, int j)
-        {
-            return (0 <= i && i < rows) 
-            && (0 <= j && j < cols)
-            ? true
-            : false;
-        };
+        CheckNextNode check = (i, j) =>
+        (0 <= i && i < rows) && (0 <= j && j < cols);
+
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
             {
                 List<(int, string)> arr = new List<(int, string)>();
-                (int di, int dj)[] sides = { (-1,0), (1,0), (0,-1), (0,1) };
-                foreach (var (di, dj) in sides) 
+                (int di, int dj)[] sides = { (-1, 0), (1, 0), (0, -1), (0, 1) };
+                foreach (var (di, dj) in sides)
                 {
-                    if (check(i + di, j + dj)) 
-                        arr.Add((Math.Abs(map[i+di, j+dj] - map[i,j]),
-                            $"[{i+di}][{j+dj}]"));
+                    if (check(i + di, j + dj))
+                        arr.Add((Math.Abs(map[i + di, j + dj] - map[i, j]),
+                            $"[{i + di}][{j + dj}]"));
                 }
-                graph.Add($"[{i}][{j}]", arr.ToArray());                
+                graph.Add($"[{i}][{j}]", arr.ToArray());
             }
         }
-        foreach (var kvp in graph)
+
+        string start = "[0][0]";
+        string end = $"[{rows - 1}][{cols - 1}]";
+        var dejkstra = Dejkstra(start, end, graph);
+        var visited = dejkstra.Item1;
+        int fuel = dejkstra.Item2;
+
+        string currNode = end;
+        string path = $"{currNode}";
+        int steps = 0;
+
+        while (currNode != start)
         {
-            Console.Write($"{kvp.Key} : ");
-            foreach (var v in kvp.Value) Console.Write(v);
-            Console.WriteLine();
+            currNode = visited[currNode];
+            path = path.Insert(0, $"{currNode}->");
+            steps++;
+        }
+
+        using (StreamWriter sw = new StreamWriter("path-plan.txt", true))
+        {
+            sw.WriteLine(path);
+            sw.WriteLine($"steps: {steps}");
+            sw.WriteLine($"fuel: {fuel}");
         }
     }
 
     static void Main(string[] args)
     {
-        CalculateRoverPath(new int[,] { 
-            { 1, 2, 3 },
-            {4,5,6 },
-            {7,8,9 }
+        CalculateRoverPath(new int[,] {
+            { 0, 2, 3, 4, 1 },
+            { 2, 3, 4, 4, 1 },
+            { 3, 4, 5, 6, 2 },
+            { 4, 5, 6, 7, 1 },
+            { 6, 7, 8, 7, 1 }
+        });
+        CalculateRoverPath(new int[,] {
+            { 0, 1, 1, 1, 0 },
+            { 1, 1, 3, 1, 1 },
+            { 0, 1, 1, 1, 0 },
+            { 0, 0, 0, 0, 0 },
         });
         Console.ReadKey();
     }
