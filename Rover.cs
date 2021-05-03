f59b54add86375b54612008c5c919f07f63d1485
Victor <vitkow@tut.by>
2021-05-03 16:30:11 +0300

    modificate Dejkstra to A*. Add class Node.

--- Rover.cs
+++ Rover.cs
@@ -2,33 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
+
+public class Node : IComparable<Node>
+{
+    public int I { get; set; }
+    public int J { get; }
+    public Node(int i, int j)
+    {
+        I = i;
+        J = j;
+    }
+    internal string Show()
+    {
+        return $"[{I}][{J}]";
+    }
+
+    public int CompareTo(Node other)
+    {
+        return I.CompareTo(other.I);
+    }
+}
+
 public class Rover
 {
-    static (Dictionary<string, string>, int) Dejkstra(string startNode, string endNode,
-        Dictionary<string, (int, string)[]> graph)
+    static int Heuristic(Node a, Node end)
     {
-        List<(int, string)> nodesQueue = new List<(int, string)>() { (0, startNode) };
-        Dictionary<string, int> fuelVisit = new Dictionary<string, int>() { { startNode, 0 } };
-        Dictionary<string, string> visitedFrom = new Dictionary<string, string>() { { startNode, null } };
+        return Math.Abs(a.I - end.I) + Math.Abs(a.J - end.J);
+    }
+
+    static (Dictionary<Node, Node>, int) Dejkstra(Node startNode, Node endNode,
+        Dictionary<Node, (int, Node)[]> graph)
+    {
+        List<(int, Node)> nodesQueue = new List<(int, Node)>() { (0, startNode) };
+        Dictionary<Node, int> fuelVisit = new Dictionary<Node, int>() { { startNode, 0 } };
+        Dictionary<Node, Node> visitedFrom = new Dictionary<Node, Node>() { { startNode, null } };
 
         while (nodesQueue.Count != 0)
         {
-            string currNode = nodesQueue[0].Item2;
-            nodesQueue.RemoveAt(0);
+            Node currNode = nodesQueue[0].Item2;
+            nodesQueue.RemoveAt(0);            
             if (currNode == endNode) break;  //end of algorithm 
 
             var nextNodes = graph[currNode];
-            foreach ((int, string) nextNode in nextNodes)
+            foreach ((int, Node) nextNode in nextNodes)
             {
                 int neighbourFuel = nextNode.Item1;
-                string neighbourNode = nextNode.Item2;
+                Node neighbourNode = nextNode.Item2;
                 var updateFuel = fuelVisit[currNode] + neighbourFuel + 1; //+1 point fuel on move
 
                 if (!fuelVisit.ContainsKey(neighbourNode) ||
                     updateFuel < fuelVisit[neighbourNode])
                 {
+                    int priority = updateFuel + Heuristic(neighbourNode, endNode); //update to *A-algorithm
                     fuelVisit[neighbourNode] = updateFuel;
-                    nodesQueue.Add((updateFuel, neighbourNode));
+                    nodesQueue.Add((priority, neighbourNode));
                     nodesQueue.Sort();
                     visitedFrom[neighbourNode] = currNode;
                 }
@@ -43,45 +70,50 @@ public class Rover
     {
         int rows = map.GetLength(0);
         int cols = map.GetLength(1);
-        Dictionary<string, (int, string)[]> graph = new Dictionary<string, (int, string)[]>();
+        Dictionary<Node, (int, Node)[]> graph = new Dictionary<Node, (int, Node)[]>();
 
         CheckNextNode check = (i, j) =>
         (0 <= i && i < rows) && (0 <= j && j < cols);
 
+        Node[,] nodes = new Node[rows,cols];
+        for(int i=0; i<rows;i++)
+            for(int j=0; j<cols;j++)
+                nodes[i, j] = new Node(i, j);
+
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
-            {
-                List<(int, string)> arr = new List<(int, string)>();
+            {               
+                List<(int, Node)> arr = new List<(int, Node)>();
                 (int di, int dj)[] sides = { (-1, 0), (1, 0), (0, -1), (0, 1) };
                 foreach (var (di, dj) in sides)
                 {
                     if (check(i + di, j + dj))
                         arr.Add((Math.Abs(map[i + di, j + dj] - map[i, j]),
-                            $"[{i + di}][{j + dj}]"));
+                            nodes[i + di, j + dj]));
                 }
-                graph.Add($"[{i}][{j}]", arr.ToArray());
+                graph.Add(nodes[i,j], arr.ToArray());
             }
         }
 
-        string start = "[0][0]";
-        string end = $"[{rows - 1}][{cols - 1}]";
+        Node start = nodes[0, 0];
+        Node end = nodes[rows - 1, cols - 1];
         var dejkstra = Dejkstra(start, end, graph);
         var visited = dejkstra.Item1;
         int fuel = dejkstra.Item2;
 
-        string currNode = end;
-        string path = $"{currNode}";
+        Node currNode = end;
+        string path = $"{currNode.Show()}";
         int steps = 0;
 
         while (currNode != start)
         {
             currNode = visited[currNode];
-            path = path.Insert(0, $"{currNode}->");
+            path = path.Insert(0, $"{currNode.Show()}->");
             steps++;
         }
 
-        using (StreamWriter sw = new StreamWriter("path-plan.txt", true))
+        using (StreamWriter sw = new StreamWriter("path-plan.txt"))
         {
             sw.WriteLine(path);
             sw.WriteLine($"steps: {steps}");
@@ -91,19 +123,5 @@ public class Rover
 
     static void Main(string[] args)
     {
-        CalculateRoverPath(new int[,] {
-            { 0, 2, 3, 4, 1 },
-            { 2, 3, 4, 4, 1 },
-            { 3, 4, 5, 6, 2 },
-            { 4, 5, 6, 7, 1 },
-            { 6, 7, 8, 7, 1 }
-        });
-        CalculateRoverPath(new int[,] {
-            { 0, 1, 1, 1, 0 },
-            { 1, 1, 3, 1, 1 },
-            { 0, 1, 1, 1, 0 },
-            { 0, 0, 0, 0, 0 },
-        });
-        Console.ReadKey();
     }
 }
